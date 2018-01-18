2e35fc8ea6a1721f98b94d5d1911d01434c72853
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-01-18 18:15:09 +0200

    Решение задания по работе с элементами матрицы.

---
+++ MyArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Collection02
+{
+    class MyArray
+    {
+        // Метод для создания массива
+        public static int[,] CreateArray()
+        {
+            Random random = new Random();
+            Console.Write("Укажите размер матрицы (m * m): ");
+            int m = int.Parse(Console.ReadLine());
+
+            // Формируем массив
+            int[,] array = new int[m, m];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 21);
+                }
+            }
+
+            return array;
+        }
+
+        // Метод, который в каждой строке меняет значение максимального
+        // элемента на сумму элементов с четными индексами
+        public static void EditArray(int[,] array)
+        {
+            int max, indexJ;
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                max = array[i, 0];
+                indexJ = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                        indexJ = j;
+                    }
+                    if (j % 2 == 0) sum += array[i, j];
+                }
+                array[i, indexJ] = sum;
+                sum = 0;
+            }
+        }
+
+        // Метод для вывода массива на консоль
+        public static void PrintArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0,2} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/* Задание: Дана матрица А, состоящая из М строк и М столбцов. 
+ * В каждой строке заменить максимальный элемент суммой элементов с четными индексами.*/
+
+namespace Collection02
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] array = MyArray.CreateArray();   // Создаем массив
+
+            Console.WriteLine("\nЗаданный массив:");
+            MyArray.PrintArray(array);              // Выводим созданный массив на консоль
+
+            MyArray.EditArray(array);               // Форматируем массив
+
+            Console.WriteLine("\nРезультирующий массив:");
+            MyArray.PrintArray(array);              // Выводим измененный массив на консоль
+
+            Console.ReadKey();
+        }
+    }
+}
